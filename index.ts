da06e42708b79b748e723560c9f95128310d2448
mykhailovz <mykhailo.vz@gmail.com>
2020-07-30 13:12:03 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -145,3 +145,35 @@ ins.testCognitiveComplexityRule(1);
 if (1 === 1) {
   console.log('More code to test');
 }
+
+if (1) {
+  if (2) {
+    if (3) {
+      if (4) {
+        if (5) {
+          if (6) {
+            if (7) {
+              if (8) {
+                if (9) {
+                  if (10) {
+                    if (11) {
+                      if (12) {
+                        if (13) {
+                          if (14) {
+                            if (15) {
+                              testCognitiveRule();
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
